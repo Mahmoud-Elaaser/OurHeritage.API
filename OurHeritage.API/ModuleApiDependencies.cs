34f855b70ead00307b796b4eae5f77de4db6cefd
unknown <mahmoudelaaser57@gmail.com>
2026-02-16 22:12:52 +0200

    refactor(program): move API dependencies out of program.cs

---
+++ OurHeritage.API/ModuleApiDependencies.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using OurHeritage.Core.Context;
+using OurHeritage.Core.Entities;
+using OurHeritage.Repo.Repositories.Implementations;
+using OurHeritage.Service.DTOs;
+using Stripe;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace OurHeritage.API
+{
+    public static class ModuleApiDependencies
+    {
+        public static void AddAuthSupport(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Type = SecuritySchemeType.Http,
+                    Description = "JWT Authorization header using the Bearer scheme"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)),
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+
+                        // Allow JWT token for SignalR NotificationHub as well
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/hubs/chat") || path.StartsWithSegments("/hubs/notification")))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+
+            });
+
+            services.Configure<StripeSettings>(configuration.GetSection("Stripe"));
+            var stripeSettings = configuration.GetSection("Stripe").Get<StripeSettings>();
+            StripeConfiguration.ApiKey = stripeSettings.SecretKey;
+
+        }
+
+
+        public static void AddCorsAndJsonSupport(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    //options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                });
+
+            services.AddSignalR();
+        }
+
+
+        
+    }
+}
--- OurHeritage.API/Program.cs
+++ OurHeritage.API/Program.cs
@@ -1,19 +1,12 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using OurHeritage.API.Hubs;
 using OurHeritage.Core.Context;
 using OurHeritage.Core.Entities;
 using OurHeritage.Repo;
 using OurHeritage.Repo.Repositories.Implementations;
 using OurHeritage.Service;
-using OurHeritage.Service.DTOs;
 using OurHeritage.Service.Helper;
-using Stripe;
-using System.Text;
-using System.Text.Json.Serialization;
 
 namespace OurHeritage.API
 {
@@ -24,135 +17,30 @@ namespace OurHeritage.API
             var builder = WebApplication.CreateBuilder(args);
 
 
-            #region Connection String & Identity
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-
-
             builder.Services.AddIdentity<User, IdentityRole<int>>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-            var stripeSettings = builder.Configuration.GetSection("Stripe").Get<StripeSettings>();
-            StripeConfiguration.ApiKey = stripeSettings.SecretKey;
-
-            #endregion
-
-            #region Register Services
+            
             builder.Services.AddServiceDependencies();
             builder.Services.AddRepoDependencies();
-            #endregion
-
-
-            builder.Services.AddSignalR();
-
+            builder.Services.AddAuthSupport(builder.Configuration);
+            builder.Services.AddCorsAndJsonSupport();
 
             builder.WebHost.UseWebRoot("wwwroot");
 
-            builder.Services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    //options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-                });
-
-
-
 
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             FilesSetting.Configure(builder.Services.BuildServiceProvider().GetRequiredService<IHttpContextAccessor>(), builder.Configuration);
 
 
-
-
-            #region Auth for swagger
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    Type = SecuritySchemeType.Http,
-                    Description = "JWT Authorization header using the Bearer scheme"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new List<string>()
-                    }
-                });
-            });
-
-            builder.Services.AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["Jwt:Audience"],
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
-                    ClockSkew = TimeSpan.Zero
-                };
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
-                        var path = context.HttpContext.Request.Path;
-
-                        // Allow JWT token for SignalR NotificationHub as well
-                        if (!string.IsNullOrEmpty(accessToken) &&
-                            (path.StartsWithSegments("/hubs/chat") || path.StartsWithSegments("/hubs/notification")))
-                        {
-                            context.Token = accessToken;
-                        }
-
-                        return Task.CompletedTask;
-                    }
-                };
-
-            });
-            #endregion
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAllOrigins",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                               .AllowAnyMethod()
-                               .AllowAnyHeader();
-                    });
-            });
-
             var app = builder.Build();
-
-
-            #region Database Initialization & Role Seeding
 
             using (var scope = app.Services.CreateScope())
             {
@@ -162,9 +50,9 @@ namespace OurHeritage.API
                 try
                 {
                     var dbContext = services.GetRequiredService<ApplicationDbContext>();
-                    await dbContext.Database.MigrateAsync(); // Apply pending migrations
+                    await dbContext.Database.MigrateAsync(); 
 
-                    await RoleInitializer.InitializeAsync(services); // Seed roles if not exists
+                    await RoleInitializer.InitializeAsync(services); 
                 }
                 catch (Exception ex)
                 {
@@ -172,7 +60,7 @@ namespace OurHeritage.API
                     logger.LogError(ex, "An error occurred while applying migrations.");
                 }
             }
-            #endregion
+
             app.UseCors("AllowAllOrigins");
             app.UseStaticFiles();
             app.UseSwagger();
